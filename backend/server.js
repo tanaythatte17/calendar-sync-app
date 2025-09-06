91d8ece54473d54a810cf0774173ec719a46929f
Tanay Thatte <tanaythatte17@gmail.com>
2025-09-07 02:23:38 +0530

    Checking deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import agenda from './utils/agendaUtils.js';
 dotenv.config();
 
 const app = express();
-
+// checking deployment
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
