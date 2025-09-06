7b07c3b767bca90c1daadd38b5a3406e6be103e2
Tanay Thatte <tanaythatte17@gmail.com>
2025-09-06 15:43:31 +0530

    Testing workflow

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import agenda from './utils/agendaUtils.js';
 dotenv.config();
 
 const app = express();
-
+//testing workflow
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
