7052077431783e3322cad68129aa3168c3d34dc3
Tanay Thatte <tanaythatte17@gmail.com>
2025-12-18 23:39:03 +0530

    Changed IP and removed comment

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -5,7 +5,6 @@ import { format, startOfWeek, endOfWeek, eachDayOfInterval} from 'date-fns';
 import { formatInTimeZone } from 'date-fns-tz';
 import { FaChevronLeft, FaChevronRight, FaCalendar, FaList, FaCalendarWeek } from 'react-icons/fa';
 
-//Addingg comments
 interface Event {
   _id: string;
   calendarAccountId: string;
